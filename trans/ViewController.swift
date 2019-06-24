942cb2d87878571655fa3481837e2c4d8c424888
張宮寶 <mars09250148@gmail.com>
2019-06-24 13:25:21 +0800

    Initial Commit

---
+++ trans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trans
+//
+//  Created by Mars on 2019/6/24.
+//  Copyright © 2019 drs24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
